3b74e96fe65043a9451ea5c30031adf2b7a2ad27
Svitlana Dyshkant <110805508+SvitlanaDy@users.noreply.github.com>
2023-02-20 19:51:09 +0700

    install package

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import './css/styles.css';
 import debounce from 'lodash.debounce';
 import Notiflix from 'notiflix';
 const DEBOUNCE_DELAY = 300;
-import fetchCountries from './js/fetchCountries';
+// import fetchCountries from './js/fetchCountries';n
